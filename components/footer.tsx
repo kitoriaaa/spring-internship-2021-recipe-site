12423d4413fb8bdd0ebd32c83a5dc028dcee62d0
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-23 02:03:44 +0900

    Add: footer component

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+type Props = {
+  footer: string
+};
+
+export const Footer: FC<Props> = (props) => {
+  return (
+    <div className="text-center h6 my-4">
+      <div>{props.footer}</div>
+    </div>
+  );
+};
