64283617cd5ef2eb30624abaad86751a87ffe438
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-23 02:14:51 +0900

    Update: Home use layout comp and display recipes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { Layout } from '../components/layout';
+import { Recipe, getRecipes } from "../lib/getRecipe";
 
-const TopPage: FC = () => {
-    return <h1>Hello Next!</h1>;
+
+const Home: FC = () => {
+  const [recipes, setRecipes] = useState<Recipe[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const recipes = await getRecipes();
+      setRecipes(recipes);
+    })();
+  }, []);
+
+  // console.log(recipes);
+  if (recipes === null) return <div>loading...</div>;
+
+  return (
+    <Layout header="Recipe" title="レシピを検索">
+      <h1>Hello Next!</h1>
+      {recipes.map((recipe) => (
+        <div className='alert alert-primary' key={recipe.id}>
+          <p className="text-center">{recipe.title}</p>
+        </div>
+      ))}
+    </Layout>
+  );
 };
 
-export default TopPage;
+export default Home;
