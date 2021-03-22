affba466f791f244782bc8dc3c158164bd07c7aa
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-23 02:04:04 +0900

    Add: layout component

---
+++ components/layout.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import Head from "next/head";
+import { Header } from "./header";
+import { Footer } from "./footer";
+
+type Props = {
+  title: string,
+  header: string,
+  children: React.ReactNode
+}
+
+
+export const Layout: FC<Props> = (props) => {
+  return (
+    <div>
+      <Head>
+        <title>{props.title}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous"></link>
+      </Head>
+      <Header header={props.header} />
+      <div className="container">
+        <h4 className="my-3 text-primary text-center">
+          {props.title}
+        </h4>
+        {props.children}
+      </div>
+      <Footer footer="copyright kitoriaaa" />
+    </div>
+  );
+
+};
