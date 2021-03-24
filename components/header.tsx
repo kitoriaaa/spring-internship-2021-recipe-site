1cd690f3eebaa8d9b6667aa7820489afc1654372
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-24 18:31:56 +0900

    Update: header link toppage

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import Link from "next/link";
 
 type Props = {
   header: string
@@ -7,8 +8,16 @@ type Props = {
 export const Header: FC<Props> = (props) => {
   return (
     <div>
-      <h1 className="bg-primary px-3 text-white display-4 text-left">
-        {props.header}
+      <h1 className="bg-primary px-3 text-white display-4 text-left text-white">
+        <div className="row">
+          <div className="col-3">
+            <Link href="/">
+              <div className="text-white">
+                {props.header}
+              </div>
+            </Link>
+          </div>
+        </div>
       </h1>
     </div>
   );
