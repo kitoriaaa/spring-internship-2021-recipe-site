4182d8aaae63450c02ac48875f2f87473c760a8b
kitoriaaa <69099518+kitoriaaa@users.noreply.github.com>
2021-03-26 02:23:26 +0900

    Fix: separate footer and navibar

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,4 @@
 import React, { FC } from "react";
-import Image from 'next/image';
-import Link from "next/link";
 
 type Props = {
   footer: string
@@ -8,30 +6,7 @@ type Props = {
 
 export const Footer: FC<Props> = (props) => {
   return (
-    <div className="text-center h6 my-4" id="footer">
-      <div className="row">
-        <Link href="/">
-          <div className="col-4" role="button">
-            <Image src="/home.svg" alt="Picture of the author"
-              width={30}
-              height={30} />
-          </div>
-        </Link>
-        <Link href="/search">
-          <div className="col-4" role="button">
-            <Image src="/search.svg" alt="Picture of the author"
-              width={30}
-              height={30} />
-          </div>
-        </Link>
-        <Link href="/fav">
-          <div className="col-4" role="button">
-            <Image src="/star.svg" alt="Picture of the author"
-              width={30}
-              height={30} />
-          </div>
-        </Link>
-      </div>
+    <div className="text-center h6 my-4">
       <hr />
       <div>{props.footer}</div>
     </div>
